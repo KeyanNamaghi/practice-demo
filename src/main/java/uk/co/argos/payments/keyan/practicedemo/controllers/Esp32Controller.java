5e3df35e32bd2c94f08db89eac5c031f538fe3b3
Keyan <keyan.namaghi@argos.co.uk>
2020-08-09 16:00:37 +0100

    removed esp

--- src/main/java/uk/co/argos/payments/keyan/practicedemo/controllers/Esp32Controller.java
+++ src/main/java/uk/co/argos/payments/keyan/practicedemo/controllers/Esp32Controller.java
@@ -1,43 +1,43 @@
-package uk.co.argos.payments.keyan.practicedemo.controllers;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import uk.co.argos.payments.keyan.practicedemo.models.Reading;
-import uk.co.argos.payments.keyan.practicedemo.repositories.Esp32Repository;
-
-import java.time.Instant;
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/v1/esp32")
-public class Esp32Controller {
-    @Autowired
-    private Esp32Repository esp32Repository;
-
-    @GetMapping
-    public List<Reading> list(){
-        return esp32Repository.findAll();
-    }
-
-    @GetMapping
-    @RequestMapping("{id}")
-    public Reading get(@PathVariable long id){
-        return esp32Repository.getOne(id);
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Reading create(@RequestBody final Reading reading){
-        Instant instant = Instant.now();
-        long timeStampSeconds = instant.getEpochSecond();
-        reading.setTimestamp(timeStampSeconds);
-        return esp32Repository.saveAndFlush(reading);
-    }
-
-    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable long id){
-        esp32Repository.deleteById(id);
-    }
-
-}
+//package uk.co.argos.payments.keyan.practicedemo.controllers;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.*;
+//import uk.co.argos.payments.keyan.practicedemo.models.Reading;
+//import uk.co.argos.payments.keyan.practicedemo.repositories.Esp32Repository;
+//
+//import java.time.Instant;
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/v1/esp32")
+//public class Esp32Controller {
+//    @Autowired
+//    private Esp32Repository esp32Repository;
+//
+//    @GetMapping
+//    public List<Reading> list(){
+//        return esp32Repository.findAll();
+//    }
+//
+//    @GetMapping
+//    @RequestMapping("{id}")
+//    public Reading get(@PathVariable long id){
+//        return esp32Repository.getOne(id);
+//    }
+//
+//    @PostMapping
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public Reading create(@RequestBody final Reading reading){
+//        Instant instant = Instant.now();
+//        long timeStampSeconds = instant.getEpochSecond();
+//        reading.setTimestamp(timeStampSeconds);
+//        return esp32Repository.saveAndFlush(reading);
+//    }
+//
+//    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+//    public void delete(@PathVariable long id){
+//        esp32Repository.deleteById(id);
+//    }
+//
+//}
--- src/main/java/uk/co/argos/payments/keyan/practicedemo/models/Reading.java
+++ src/main/java/uk/co/argos/payments/keyan/practicedemo/models/Reading.java
@@ -1,55 +1,55 @@
-package uk.co.argos.payments.keyan.practicedemo.models;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import java.util.List;
-
-@Entity(name="reading")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-public class Reading {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long reading_id;
-    private Integer value;
-    private Long timestamp;
-
-    @ManyToMany(mappedBy = "readings")
-    @JsonIgnore
-    private List<Reading> readings;
-
-    public Reading(){}
-
-    public List<Reading> getReadings() {
-        return readings;
-    }
-    public void setReadings(List<Reading> readings) {
-        this.readings = readings;
-    }
-
-    public Long getReading_id() {
-        return reading_id;
-    }
-
-    public Integer getValue() {
-        return value;
-    }
-
-    public void setValue(Integer value) {
-        this.value = value;
-    }
-
-    public Long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Long timestamp) {
-        this.timestamp = timestamp;
-    }
-}
+//package uk.co.argos.payments.keyan.practicedemo.models;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.ManyToMany;
+//import java.util.List;
+//
+//@Entity(name="reading")
+//@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+//public class Reading {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long reading_id;
+//    private Integer value;
+//    private Long timestamp;
+//
+//    @ManyToMany(mappedBy = "readings")
+//    @JsonIgnore
+//    private List<Reading> readings;
+//
+//    public Reading(){}
+//
+//    public List<Reading> getReadings() {
+//        return readings;
+//    }
+//    public void setReadings(List<Reading> readings) {
+//        this.readings = readings;
+//    }
+//
+//    public Long getReading_id() {
+//        return reading_id;
+//    }
+//
+//    public Integer getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(Integer value) {
+//        this.value = value;
+//    }
+//
+//    public Long getTimestamp() {
+//        return timestamp;
+//    }
+//
+//    public void setTimestamp(Long timestamp) {
+//        this.timestamp = timestamp;
+//    }
+//}
