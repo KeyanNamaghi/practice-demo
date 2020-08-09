b006ad85f66771af6877d3cc6a53ea501dfbc2b1
Keyan <keyan.namaghi@argos.co.uk>
2020-08-09 15:08:09 +0100

    esp added

---
+++ src/main/java/uk/co/argos/payments/keyan/practicedemo/repositories/Esp32Repository.java
@@ -0,0 +1,7 @@
+package uk.co.argos.payments.keyan.practicedemo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uk.co.argos.payments.keyan.practicedemo.models.Reading;
+
+public interface Esp32Repository extends JpaRepository<Reading, Long> {
+}
